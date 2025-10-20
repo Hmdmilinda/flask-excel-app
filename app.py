efe1ab39b92129bac4cba9c61dbfa6a7898e241d
Hmdmilinda <hmd.milinda@gmail.com>
2025-10-20 22:14:43 +0530

    Refactor Google Sheets credentials handling

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os
+import json
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Set up Google Sheets connection
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds_json = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
 creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("App_Data").sheet1
